0f56b905ac482167a8df8f6d25e821b2f9af57d7
elobaton <138057675+elobaton@users.noreply.github.com>
2023-08-04 23:14:29 +0000

    primer commmit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["El perro", "Mi abuela", "La tortuga", "Mi tia"];
+let action = ["se comió", "se orinó", "aplastó", "rompió"];
+let what = ["mi tarea", "las llaves", "mi ferrari", "mi proyecto"];
+let when = ["antes de clases", "justo a tiempo", "cuando dormía", "ayer"];
+
+function excuse(who, action, what, when) {
+  let whoExcuse = Math.floor(Math.random() * who.length);
+  let actionExcuse = Math.floor(Math.random() * action.length);
+  let whatExcuse = Math.floor(Math.random() * what.length);
+  let whenExcuse = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoExcuse] +
+    " " +
+    action[actionExcuse] +
+    " " +
+    what[whatExcuse] +
+    " " +
+    when[whenExcuse]
+  );
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excusa = excuse(who, action, what, when);
+  let excuseElement = document.getElementById("excuse");
+  excuseElement.innerHTML = excusa;
 };
